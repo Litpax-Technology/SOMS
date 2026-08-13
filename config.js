66464fc6ab1a08d5e281ccfcd7ea2c624a28a3fb
Litpax-Technology <mis@litpaxtechnology.com>
2026-08-13 16:15:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * (Deploy > Web app > Execute as: Me > Access: Anyone > copy /exec URL)
  * ========================================================= */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyms6Z19mbJA79ZU1d2i5w5j1xVu_FpuUpvAUl0CJur-kzSgHv0Rsjdq-noilKV5fJvNA/exec",   // <-- replace this
+  API_URL: "https://script.google.com/macros/s/AKfycbxZk_eAfQxJf01Rpl_d8R4pgX4lJwwSv4I4ethvvSCCmQAUqmADKx-Fuz9X6b-iYlt5Vg/exec",   // <-- replace this
   APP_NAME: "Litpax SMS",
   APP_SUBTITLE: "Store Management System",
   COMPANY: "Litpax Technology",
